0ecbea07fe469e9a3700474df6037317f61a2590
abhinav042 <abhinav.goyal@live.com>
2017-08-30 04:35:27 +0530

    adding comments for clarity :+1:

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,55 @@
-const ul = document.getElementById("images");
-const url = "https://randomuser.me/api/?results=5";
+const ul = document.getElementById("images"); //getting the unordered list
+const url = "https://randomuser.me/api/?results=5"; //the API url
 const images = [];
-const slides = Array.from(document.querySelectorAll(".mySlides"));
-const modal = document.getElementById("customModal");
-let modalImg = document.getElementById("imgAdd");
-const closeButton = document.querySelector(".close");
-let imageList = {};
+const slides = Array.from(document.querySelectorAll(".mySlides")); //images for the carousel 
+const modal = document.getElementById("customModal"); //the modal
+const closeButton = document.querySelector(".close"); //close button
+let imageList = {}; //a nodelist with all the images
 let imgIdx;
-let counter = 0;
+let counter = 0; //to keep track of the image displayed in the lightbox
+
+//opening the modal
 function openDialog() {
-	// console.log(modal);
-	// console.log(img);
-	//opening the modal
+
+	// --- DEBUGGING --- console.log(modal); --- DEBUGGING //
+
+	// --- DEBUGGING --- console.log(img); --- DEBUGGING //
 	modal.style.display = "block";
-	// modalImg.src = img.src;
 }
 
+//closing the modal
 closeButton.addEventListener("click", () => {
 	modal.style.display = "none";
 });
 
+//displaying the required image in the lightbox
 function openCarouselSlider(imgNum, prevNext = 0) {
-	console.log(parseInt(imgNum) + prevNext);
+
+	// --- DEBUGGING --- console.log(parseInt(imgNum) + prevNext); --- DEBUGGING //
 	imgNum = parseInt(imgNum);
-	// console.log(slides);
-	if (imgNum + prevNext > slides.length) imgIdx = 1;
-	else if (imgNum + prevNext < 1) imgIdx = slides.length;
-	else if (imgNum + prevNext <= slides.length && imgNum + prevNext >= 1) imgIdx = imgNum + prevNext;
-	slides[imgIdx - 1].style.display = "block";
-	console.log(imgIdx);
-	console.log(slides[imgIdx - 1]);
+
+	// --- DEBUGGING --- console.log(slides); --- DEBUGGING //
+	if (imgNum + prevNext > slides.length) imgIdx = 1; //go to the first image from the last one
+	else if (imgNum + prevNext < 1) imgIdx = slides.length; //go back to the last img from the first one
+	else if (imgNum + prevNext <= slides.length && imgNum + prevNext >= 1) imgIdx = imgNum + prevNext; //go to the next img
+	slides[imgIdx - 1].style.display = "block"; //show the image
+
+	// --- DEBUGGING --- console.log(imgIdx); --- DEBUGGING //
+
+	// --- DEBUGGING --- console.log(slides[imgIdx - 1]); --- DEBUGGING //
+	//hiding rest of the images
 	slides.forEach((slide, i) => {
 		if (i != imgIdx - 1) slide.style.display = "none";
 	});
 }
 
-function plusSlides(prevNext) {
+//to move to the next slide - pass 1
+//to move to the prev slide - pass -1
+function changeSlides(prevNext) {
 	openCarouselSlider(imgIdx, prevNext);
 }
 
+//
 function addNewImage(author) {
 	counter++;
 	const img = document.createElement("img");
@@ -48,7 +59,8 @@ function addNewImage(author) {
 	img.id = `${counter}`;
 	img.className = `addImgSrc`;
 	img.addEventListener("click", () => {
-		console.log(img.id);
+
+		// --- DEBUGGING --- console.log(img.id); --- DEBUGGING //
 		openDialog();
 		openCarouselSlider(img.id);
 	});
@@ -56,11 +68,12 @@ function addNewImage(author) {
 	ul.appendChild(li);
 }
 
+//fetching the images from the API and displaying them on the homepage
 function getImages() {
 	return fetch(url).then(res => res.json()).then(data => {
 		const imageData = data.results;
 		return imageData.map(author => {
-			//
+			//adding properties to the images and appending them on the page
 			addNewImage(author);
 		});
 	});
@@ -70,9 +83,11 @@ function getAllTheImages() {
 	return Promise.all([getImages()]);
 }
 
+//doing tasks after all promises have been resolved/rejected
 getAllTheImages().then(() => {
 	imageList = [...document.querySelectorAll(".addImgSrc")];
-	// console.log(imageList);
+
+	// --- DEBUGGING --- console.log(imageList); --- DEBUGGING //
 	for (let i = 0; i < imageList.length; i++) {
 		console.log(document.getElementById(`image0${i+1}`).src);
 		document.getElementById(`image0${i+1}`).src = imageList[i].src;
